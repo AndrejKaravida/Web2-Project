ee4c1a8e2cc3050d37e25ecd266ef9586aa4856a
vujadinjanjic <57356499+vujadinjanjic@users.noreply.github.com>
2020-04-05 23:49:43 +0200

    merge

--- WEB2Project-SPA/src/app/_services/avio.service.ts
+++ WEB2Project-SPA/src/app/_services/avio.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Destination } from '../_models/destination';
 import { Observable } from 'rxjs';
 import { AvioCompany } from '../_models/aviocompany';
+import { Flight } from '../_models/flight';
+import { faUnderline } from '@fortawesome/free-solid-svg-icons';
 
 @Injectable({
   providedIn: 'root'
@@ -22,16 +24,24 @@ export class AvioService {
     return this.http.get<AvioCompany>(this.baseUrl + 'avio/getCompany/' + id);
   }
 
-  getAllDestinationsForCompany(id: number): Observable<Destination[]> {
-    return this.http.get<Destination[]>(this.baseUrl + 'avio/destinations/' + id);
-  }
-
   getAllAvioCompanies(): Observable<AvioCompany[]> {
     return this.http.get<AvioCompany[]>(this.baseUrl + 'avio/aircompanies/');
   }
 
- 
+  getFlightsForCompany(companyId, flightParams?): Observable<Flight[]>{
 
+    let params = new HttpParams();
 
+    if(flightParams != null && flightParams != faUnderline) { 
+      params = params.append('minPrice', flightParams.minPrice);
+      params = params.append('maxPrice', flightParams.maxPrice);
+      params = params.append('departureDestination', flightParams.departureDestination);
+      params = params.append('arrivalDestination', flightParams.arrivalDestination);
+      params = params.append('departureDate', flightParams.departureDate);
+      params = params.append('returningDate', flightParams.returningDate);
+    }
+
+    return this.http.get<Flight[]>(this.baseUrl + 'avio/getFlights/' + companyId, {params});
+  }
 
 }
