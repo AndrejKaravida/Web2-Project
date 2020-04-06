4e3f29b504c421b9ac6af067e93d7160a4e5222a
vujadinjanjic <57356499+vujadinjanjic@users.noreply.github.com>
2020-04-07 00:44:13 +0200

    ratings

---
+++ WEB2Project.API/Migrations/20200406221450_added_average_grade.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEB2Project.Migrations
+{
+    public partial class added_average_grade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageGrade",
+                table: "Flights",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageGrade",
+                table: "Flights");
+        }
+    }
+}
